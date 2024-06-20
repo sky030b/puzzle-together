9918ee2eeab5738283314ea82816633fb6aad160
sky030b <sky030blue030@gmail.com>
2024-06-20 17:56:05 +0800

    Feat: add JWT token return format

--- server/services/playerDatabase.js
+++ server/services/playerDatabase.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const pool = require('./createDatabasePool');
 
 async function getPlayerById(id) {
@@ -32,6 +33,31 @@ async function getAnonymousNickname() {
   return animal;
 }
 
+function getPlayerToken(player) {
+  try {
+    const accessExpired = 4 * 60 * 60;
+    const options = {
+      expiresIn: accessExpired
+    };
+
+    const {
+      player_id: playerId, email, nickname, represent_color: representColor
+    } = player;
+
+    const playerInfo = {
+      playerId, email, nickname, representColor
+    };
+
+    const accessToken = jwt.sign(playerInfo, process.env.JWT_PRIVATE_KEY, options);
+    const data = { accessToken, accessExpired, playerInfo };
+
+    return { data };
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
 async function addNewPlayer(info) {
   const {
     playerId, email, hashedPassword, nickname, representColor, isRoomPublic
@@ -50,7 +76,7 @@ async function addNewPlayer(info) {
   `, playerInfo);
 
   const newPlayer = await getPlayerById(id);
-  return newPlayer;
+  return getPlayerToken(newPlayer);
 }
 
 module.exports = {
