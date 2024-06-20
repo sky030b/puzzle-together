5b7ba16943278c26c5c5de71ce744fe515ae4cef
sky030b <sky030blue030@gmail.com>
2024-06-20 21:01:05 +0800

    Feat: add try catch block to the asynchronous function

--- server/public/player.js
+++ server/public/player.js
@@ -2,36 +2,65 @@
 import { getCookie, setCookie } from './utils.js';
 
 async function getAnonymousPlayerInfo() {
-  const res = await axios.get('/api/1.0/players/anonymous');
-  return res.data;
+  try {
+    const res = await axios.get('/api/1.0/players/anonymous');
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
 
 async function getPlayerInfoByToken() {
-  const res = await axios.get('/api/1.0/players/playerInfo');
-  return res.data;
+  try {
+    const res = await axios.get('/api/1.0/players/playerInfo');
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
 
 export async function getPlayerInfo() {
-  const playerInfo = getCookie('playerInfo');
-  if (playerInfo) return JSON.stringify(playerInfo);
+  try {
+    const playerInfo = getCookie('playerInfo');
+    if (playerInfo) return JSON.stringify(playerInfo);
 
-  let fetchedPlayerInfo;
-  const token = getCookie('token');
-  if (token) {
-    fetchedPlayerInfo = await getPlayerInfoByToken();
-  } else {
-    fetchedPlayerInfo = await getAnonymousPlayerInfo();
+    let fetchedPlayerInfo;
+    const token = getCookie('token');
+    if (token) {
+      fetchedPlayerInfo = await getPlayerInfoByToken();
+    } else {
+      fetchedPlayerInfo = await getAnonymousPlayerInfo();
+    }
+    return fetchedPlayerInfo;
+  } catch (error) {
+    console.error(error);
+    return error;
   }
-  return fetchedPlayerInfo;
 }
 
 export async function setPlayerInfo() {
-  const playerInfo = await getPlayerInfo();
-  setCookie('playerInfo', JSON.stringify(playerInfo));
+  try {
+    const playerInfo = await getPlayerInfo();
+    if (playerInfo instanceof Error) throw playerInfo;
+
+    setCookie('playerInfo', JSON.stringify(playerInfo));
+    return 'setPlayerInfo Done.';
+  } catch (error) {
+    // 告知請重新登入
+    console.error(error);
+    return error;
+  }
 }
 
 export async function playerInit() {
-  const playerInfo = getCookie('playerInfo');
-
-  if (!playerInfo) await setPlayerInfo();
+  try {
+    const playerInfo = getCookie('playerInfo');
+    if (!playerInfo) await setPlayerInfo();
+    return 'playerInit Done.';
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
