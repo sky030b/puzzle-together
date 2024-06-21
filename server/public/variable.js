4554b0497c016bc10705a4f4717b970046508ec8
sky030b <sky030blue030@gmail.com>
2024-06-21 16:02:12 +0800

    Feat: add new playerState variable and rename the old state variable to playgroundState

--- server/public/variable.js
+++ server/public/variable.js
@@ -1,24 +1,41 @@
-const state = {
-  scale: 1,
-  currentGameId: ''
-};
-
 export const maxDimension = 1500;
 export const canvasWidth = 16000;
 export const canvasHeight = 12000;
 
+const playgroundState = {
+  scale: 1,
+  currentGameId: ''
+};
+
 export function setScale(newScale) {
-  state.scale = newScale;
+  playgroundState.scale = newScale;
 }
 
 export function getScale() {
-  return state.scale;
+  return playgroundState.scale;
 }
 
 export function setCurrentGameId(newGameId) {
-  state.currentGameId = newGameId;
+  playgroundState.currentGameId = newGameId;
 }
 
 export function getCurrentGameId() {
-  return state.currentGameId;
+  return playgroundState.currentGameId;
+}
+
+const playerState = {
+  playerId: '',
+  nickname: '',
+  representColor: ''
+};
+
+export function setPlayerState(playerInfo) {
+  const { playerId, nickname, representColor } = playerInfo;
+  if (playerId) playerState.playerId = playerId;
+  if (nickname) playerState.nickname = nickname;
+  if (representColor) playerState.representColor = representColor;
+}
+
+export function getPlayerState() {
+  return { ...playerState };
 }
