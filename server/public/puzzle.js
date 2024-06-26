65d480cfd5f70f929af81af3f22e890334bcf59e
sky030b <sky030blue030@gmail.com>
2024-06-26 16:03:07 +0800

    Feat: play a sound effect when grabbing a puzzle from someone else's hand
    1.Feat: play one of two sound effects when grabbing up a puzzle
    2. Fix: fix the situation where sometimes querySelector cannot be successfully used due to random IDs
    FYI: change from querySelector to use getElementById

--- server/public/puzzle.js
+++ server/public/puzzle.js
@@ -184,6 +184,11 @@ export function addDragAndDrop(gameInfo) {
       lastNotLockedPiece = null;
     }
 
+    const audioFiles = ['mine_in_chinese.mp3', 'mine.mp3'];
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+    const player = new Audio(`https://dsz5eydy8se7.cloudfront.net/${audioFiles[randomIndex]}`);
+    player.play();
+
     selectedPiece = e.target;
     socket.emit('changeMoveBy', { gameId: getCurrentGameId(), puzzleId: selectedPiece.id, moveBy: getPlayerState().nickname });
     selectedPiece.style.zIndex = '10';
