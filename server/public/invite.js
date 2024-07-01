da6dd1eeebe529e742f928ce3a61cd3cf50b298a
sky030b <sky030blue030@gmail.com>
2024-07-01 14:56:00 +0800

    Feat: completed invite players to join the game API connection

--- server/public/invite.js
+++ server/public/invite.js
@@ -1,10 +1,24 @@
-import { sendInviteBtn } from "./dom";
+import { sendInviteBtn } from './dom.js';
+import { getCurrentGameId, getPlayerState } from './variable.js';
 
 const inviteForm = document.querySelector('#invite-form');
-const sendPlayerInvite = (e) => {
+const sendPlayerInvite = async (e) => {
   e.preventDefault();
-  alert('邀請成功：' + inviteForm[0].value);
-  inviteForm.reset();
+  try {
+    const { playerId: inviterId } = getPlayerState();
+    if (!inviterId) return alert('請先登入才可邀請其他玩家一同遊玩！');
+    const inviteInfo = {
+      inviterId,
+      inviteeId: inviteForm[0].value.trim(),
+      gameId: getCurrentGameId()
+    }
+    const res = await axios.post('/api/1.0/players/invite', inviteInfo);
+    console.log(res.data);
+    alert('邀請成功：' + inviteForm[0].value);
+    inviteForm.reset();
+  } catch (error) {
+    alert(error.response.data);
+  }
 };
 inviteForm.addEventListener('submit', sendPlayerInvite);
 
