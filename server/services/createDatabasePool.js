30a4921e15c05ca1827dafb3860fcc765b51e02f
sky030b <sky030blue030@gmail.com>
2024-07-04 16:26:05 +0800

    Feat: move the local mysql data of ec2 to aws RDS and change the connection to RDS

--- server/services/createDatabasePool.js
+++ server/services/createDatabasePool.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql2/promise');
 
+// for localhost
+// const pool = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE
+// });
+
+// for rds
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: process.env.RDS_MYSQL_HOST,
+  user: process.env.RDS_MYSQL_USER,
+  password: process.env.RDS_MYSQL_PASSWORD,
+  database: process.env.RDS_MYSQL_DATABASE
 });
 
 module.exports = pool;
