521b815f99a0877ed23d0d5342e95fa9dc2ccf8a
sky030b <sky030blue030@gmail.com>
2024-07-11 10:05:35 +0800

    Modify: If it is the player's own page, ToggleButtonBar will appear. If it is other players' pages, there will only be a showcase page without ToggleButtonBar.

--- client/src/components/ToggleButtonBar.jsx
+++ client/src/components/ToggleButtonBar.jsx
@@ -15,8 +15,9 @@ const ToggleButtonBar = () => {
   const isOwner = playerInfo && playerInfo.playerId === playerId;
 
   return (
-    <div className="btn-group mx-auto w-50 d-flex justify-content-center" role="group" aria-label="Basic radio toggle button group">
-      {/* <input
+    isOwner ? (
+      <div className="btn-group mx-auto w-50 d-flex justify-content-center" role="group" aria-label="Basic radio toggle button group">
+        {/* <input
         type="radio"
         className="btn-check"
         name="btnradio"
@@ -27,33 +28,30 @@ const ToggleButtonBar = () => {
       />
       <label className="btn btn-outline-primary" htmlFor="btnradio1">動態牆</label> */}
 
-      <input
-        type="radio"
-        className="btn-check"
-        name="btnradio"
-        id="btnradio2"
-        autoComplete="off"
-        checked={location.pathname.includes(`/profile/${playerId}/showcase`)}
-        onChange={() => handleNavigation(`/profile/${playerId}/showcase`)}
-      />
-      <label className="btn btn-outline-warning" htmlFor="btnradio2">展示牆</label>
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio2"
+          autoComplete="off"
+          checked={location.pathname.includes(`/profile/${playerId}/showcase`)}
+          onChange={() => handleNavigation(`/profile/${playerId}/showcase`)}
+        />
+        <label className="btn btn-outline-warning" htmlFor="btnradio2">展示牆</label>
 
-      {isOwner && (
-        <>
-          <input
-            type="radio"
-            className="btn-check"
-            name="btnradio"
-            id="btnradio3"
-            autoComplete="off"
-            checked={location.pathname.includes(`/profile/${playerId}/my-own-games`)}
-            onChange={() => handleNavigation(`/profile/${playerId}/my-own-games`)}
-          />
-          <label className="btn btn-outline-success" htmlFor="btnradio3">我創的遊戲</label>
-        </>
-      )}
+        <input
+          type="radio"
+          className="btn-check"
+          name="btnradio"
+          id="btnradio3"
+          autoComplete="off"
+          checked={location.pathname.includes(`/profile/${playerId}/my-own-games`)}
+          onChange={() => handleNavigation(`/profile/${playerId}/my-own-games`)}
+        />
+        <label className="btn btn-outline-success" htmlFor="btnradio3">我創的遊戲</label>
 
-    </div>
+      </div>
+    ) : <></>
   );
 };
 
