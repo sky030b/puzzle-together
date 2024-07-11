53d69df7a10934a94554d81af23d6e593839f358
sky030b <sky030blue030@gmail.com>
2024-07-11 09:46:46 +0800

    Modify: Set the global variable of the current scale to 0.1 when viewing playback
    FYI: This way the player won't have a gap when using the scroll wheel later.

--- server/public/result.js
+++ server/public/result.js
@@ -6,7 +6,7 @@ import { delay } from './utils.js';
 import {
   CANVAS_HEIGHT, CANVAS_WIDTH,
   getCurrentGameId, getPlayerState, getPlaygroundState,
-  getPlaygroundStateByKey, setIsModalOpen
+  getPlaygroundStateByKey, setIsModalOpen, setScale
 } from './variable.js';
 
 function lockAllPuzzles() {
@@ -34,6 +34,7 @@ function removeInviteNavItem() {
 }
 
 async function playbackGame() {
+  setScale(0.1);
   canvas.style.transform = `scale(0.1)`;
   constrainCanvas();
   centerView();
