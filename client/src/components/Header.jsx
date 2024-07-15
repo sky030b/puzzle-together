73fe39d76ab05fac6e292b346d1a40e5683f15d9
sky030b <sky030blue030@gmail.com>
2024-07-15 10:33:11 +0800

    Feat: add all-games nav-item at header
    1. Modify: change the signin/signup elements in the header to a button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,33 +28,43 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container-fluid justify-content-between">
         <NavLink className="navbar-brand" to="/">
-          <img src="https://dsz5eydy8se7.cloudfront.net/puzzle-together.svg" alt="logo" width="30" height="24" className="d-inline-block align-text-top" />
-          帕索兔蓋德
+          <div className='d-flex align-items-center'>
+            <img src="https://dsz5eydy8se7.cloudfront.net/puzzle-together.svg" alt="logo" width="30" height="24" className="d-inline-block align-text-top" />
+            <div>帕索兔蓋德</div>
+          </div>
         </NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+            <li className="nav-item">
+              <NavLink className="nav-link cursor-pointer" aria-current="page" to="/all-games">關卡總覽</NavLink>
+            </li>
+
             {isAuthenticated ? (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/create-game">創建遊戲</NavLink>
+                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/create-game">創建關卡</NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink className="nav-link cursor-pointer" aria-current="page" to={`/profile/${playerInfo.playerId}`}>玩家檔案</NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link signout-btn cursor-pointer" onClick={handleSignOut}>登出</NavLink>
+                  <NavLink className="nav-link signout-btn cursor-pointer" aria-current="page" to="/signin" onClick={handleSignOut}>登出</NavLink>
                 </li>
               </>
             ) : (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/signup">註冊</NavLink>
+                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/signup">
+                    <button type="button" className="btn btn-warning px-4">註冊</button>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/signin">登入</NavLink>
+                  <NavLink className="nav-link cursor-pointer" aria-current="page" to="/signin">
+                    <button type="button" className="btn btn-outline-warning px-4">登入</button>
+                  </NavLink>
                 </li>
               </>
             )}
