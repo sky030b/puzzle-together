75e440283c49e2c8f40866eb6557b432efe8802b
sky030b <sky030blue030@gmail.com>
2024-07-16 15:00:49 +0800

    Modify: add a period after the error message of the middleware that verifies signin/signup

--- server/middleware/validateSigninMiddleware.js
+++ server/middleware/validateSigninMiddleware.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require('express-validator');
 const validateSigninMiddleware = [
   body('email')
     .isEmail()
-    .withMessage('請提供有效的電子郵件地址'),
+    .withMessage('請提供有效的電子郵件地址。'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
