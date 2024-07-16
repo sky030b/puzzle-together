82e830fa78588b770f713f563d172ff1db85a914
sky030b <sky030blue030@gmail.com>
2024-07-16 12:45:14 +0800

    Feat: enable CORS on the server side to allow requests from CDN domain names

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
@@ -11,6 +12,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors({
+  origin: 'https://puzzle-together.e055339.com'
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
