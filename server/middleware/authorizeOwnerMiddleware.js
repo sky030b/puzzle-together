9e9c08e694d7ecd01c781c9d9e8aa5a3896f5c3b
sky030b <sky030blue030@gmail.com>
2024-07-22 19:58:02 +0800

    Refactor: adjust the error return mechanism of authorizeOwnerMiddleware to check whether the game exists

--- server/middleware/authorizeOwnerMiddleware.js
+++ server/middleware/authorizeOwnerMiddleware.js
@@ -6,12 +6,6 @@ const authorizeOwnerMiddleware = async (req, res, next) => {
     const { playerId } = res.locals.jwtData;
 
     const ownerId = await getGameOwnerIdByGameId(gameId);
-    if (ownerId instanceof Error) {
-      if (ownerId.message === '找不到指定關卡的資訊。') {
-        return res.status(404).send('404 Not Found: 找不到指定關卡的資訊。');
-      }
-      throw ownerId;
-    }
 
     if (ownerId !== playerId) {
       return res.status(403).send('403 Forbidden: 您無權限訪問此資源。');
@@ -19,7 +13,8 @@ const authorizeOwnerMiddleware = async (req, res, next) => {
 
     return next();
   } catch (error) {
-    return next(error);
+    if (error.message === '找不到指定關卡的資訊。') return res.status(404).send('404 Not Found: 找不到指定關卡的資訊。');
+    return res.status(500).send(error.message);
   }
 };
 
