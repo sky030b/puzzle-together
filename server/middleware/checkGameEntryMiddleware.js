8af58f4201220361ca9dcf60f84db03655639de8
sky030b <sky030blue030@gmail.com>
2024-07-22 19:55:20 +0800

    Refactor: adjust the error return mechanism of checkGameEntryMiddleware to check whether the game exists

--- server/middleware/checkGameEntryMiddleware.js
+++ server/middleware/checkGameEntryMiddleware.js
@@ -3,20 +3,24 @@ const { checkoutInvited } = require('../services/playerGameDatabase');
 const authenticateToken = require('./authenticateToken');
 
 const checkGameEntryMiddleware = async (req, res, next) => {
-  const { gameId } = req.params;
+  try {
+    const { gameId } = req.params;
 
-  const isPublic = await getGamePublicInfo(gameId);
-  if (isPublic instanceof Error) return res.status(400).send('400 Bad Request: 找不到指定關卡的資訊。');
-  if (isPublic) return next();
+    const isPublic = await getGamePublicInfo(gameId);
+    if (isPublic) return next();
 
-  return authenticateToken(req, res, async (err) => {
-    if (err) return next(err);
+    return authenticateToken(req, res, async (err) => {
+      if (err) return next(err);
 
-    const { playerId } = res.locals.jwtData;
-    const linkRecord = await checkoutInvited(playerId, gameId);
-    if (!linkRecord.length) return res.status(403).send('403 Forbidden: 您沒有權限存取此遊戲關卡。請確保您已經收到邀請。');
-    return next();
-  });
+      const { playerId } = res.locals.jwtData;
+      const linkRecord = await checkoutInvited(playerId, gameId);
+      if (!linkRecord.length) return res.status(403).send('403 Forbidden: 您沒有權限存取此遊戲關卡。請確保您已經收到邀請。');
+      return next();
+    });
+  } catch (error) {
+    if (error.message === '找不到指定關卡的資訊。') return res.status(404).send('404 Not Found: 找不到指定關卡的資訊。');
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = checkGameEntryMiddleware;
