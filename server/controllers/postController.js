b7d43d00008be71c722c3fa10cf65899cbef579f
sky030b <sky030blue030@gmail.com>
2024-07-22 19:53:51 +0800

    Refactor: remove unnecessary try catch block in postDatabase.js

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,10 +3,7 @@ const { addNewPost, getAllPostByPlayerId } = require('../services/postDatabase')
 async function getAllPlayerPosts(req, res) {
   try {
     const { playerId } = req.params;
-
     const allPosts = await getAllPostByPlayerId(playerId);
-    if (allPosts instanceof Error) throw allPosts;
-
     return res.status(200).send(allPosts);
   } catch (error) {
     return res.status(500).send(error.message);
@@ -18,10 +15,7 @@ async function createNewPost(req, res) {
     const { gameId, content } = req.body;
     const { playerId } = res.locals.jwtData;
     const postInfo = { playerId, gameId, content };
-
     const newGame = await addNewPost(postInfo);
-    if (newGame instanceof Error) throw newGame;
-
     return res.status(200).send(newGame);
   } catch (error) {
     return res.status(500).send(error.message);
